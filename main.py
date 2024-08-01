a31c0bfd80c6fb43884ff76f94c6b236585b51ee
naumanrafique12 <naumanrafique10.com>
2024-08-02 02:43:19 +0500

    final

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def sub(a,b):a-b
-print(sub(20,3))
-print(add(2,3))
+
+def sub(a,b):
+    return a-b
+
+
+print(sub(20, 3))
+print(add(2, 3))
+
